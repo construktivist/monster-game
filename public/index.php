41b6704f5121645eab1406a8907ee71a64addc57
Keegan Kelly <keegankelly1@gmail.com>
2017-03-13 23:40:15 -0500

    Setting up inital views for creating monsters

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Monster Game</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/styles.css" />
+  </head>
+  <body>
+    <div class="container">
+      <?php
+        require "../views/content.php"
+      ?>
+    </div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ views/content.php
@@ -0,0 +1,8 @@
+<div class="jumbotron">
+  <h2>The Monster Game</h2>
+</div>
+
+<div Class="row">
+  <button type="button" class="btn btn-primary">Create Monster</button>
+
+</div>
